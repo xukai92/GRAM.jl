4dfbdbfd6aa0f41e7cd8906ea3f3129082bcd2fb
Kai Xu <xukai921110@gmail.com>
2019-11-10 20:42:31 +0000

    attempts for cifar10

--- src/RMMMDNets.jl
+++ src/RMMMDNets.jl
@@ -1,14 +1,19 @@
 __precompile__(false)
 module RMMMDNets
 
-using Statistics, LinearAlgebra, StatsFuns, Distributions, Humanize, Dates, Flux.Data.MNIST
+using Statistics, LinearAlgebra, StatsFuns, Distributions, Humanize, Dates
+using MLDatasets: CIFAR10, MNIST
 using Random: MersenneTwister, shuffle
+using Reexport: @reexport
 
-using MLToolkit
-using MLToolkit.Neural: IntIte, Tracker
-import MLToolkit.Neural: evaluate, update!
-import MLToolkit: parse_toml, process_argdict, NeuralSampler
+@reexport using MLToolkit
+using MLToolkit.Neural: IntIte
+import MLToolkit.Neural:  evaluate, update!
+import MLToolkit: parse_toml, process_argdict, NeuralSampler, plot!
 export DataLoader
+
+Flux.Zygote.@nograd Flux.conv_transpose_dims
+Flux.Zygote.@nograd Flux.gpu
 
 ### Scripting
 
@@ -30,18 +35,69 @@ export parse_toml, process_argdict
 
 ### Neural sampler
 
+using MLToolkit.Neural: optional_BatchNorm
+
+# Flux.trainable(c::Conv) = (c.weight,)
+# Flux.trainable(ct::ConvTranspose) = (ct.weight,)
+
+function build_conv_outmnist(Din::Int, σs; isnorm::Bool=false)
+    @assert length(σs) == 4 "Length of `σs` must be `4` for `build_conv_outmnist`"
+    return Chain(
+        #     Din x B
+        Dense(Din,  1024), optional_BatchNorm(1024, σs[1], isnorm),
+        #    1024 x B
+        Dense(1024, 6272),
+        # -> 6272 x B
+        x -> reshape(x, 7, 7, 128, last(size(x))), optional_BatchNorm(128, σs[2], isnorm),
+        # ->  7 x  7 x 128 x B
+        ConvTranspose((4, 4), 128 => 64; stride=(2, 2), pad=(1, 1)), optional_BatchNorm(64, σs[3], isnorm),
+        # -> 14 x 14 x  64 x B
+        ConvTranspose((4, 4),  64 =>  1; stride=(2, 2), pad=(1, 1)), x -> σs[4].(x)
+        # -> 28 x 28 x 1 x B
+    )
+end
+
+build_conv_outmnist(Din::Int, σ::Function, σlast::Function; kwargs...) = build_conv_outmnist(Din, (σ, σ, σ, σlast); kwargs...)
+
+function build_convnet_outcifar(Din::Int, σs; isnorm::Bool=false)
+    @assert length(σs) == 4 "Length of `σs` must be `4` for `build_convnet_outcifar10`"
+    return Chain(
+        #     Din x B
+        Dense(Din,  2048), 
+        # -> 2048 x B
+        x -> reshape(x, 4, 4, 128, size(x, 2)), optional_BatchNorm(128, σs[1], isnorm; momentum=9f-1),
+        # ->  4 x  4 x 128 x B
+        ConvTranspose((4, 4), 128 => 64; stride=(2, 2), pad=(1, 1)), optional_BatchNorm(64, σs[2], isnorm; momentum=9f-1),
+        # ->  8 x  8 x  64 x B        
+        ConvTranspose((4, 4),  64 => 32; stride=(2, 2), pad=(1, 1)), optional_BatchNorm(32, σs[3], isnorm; momentum=9f-1),
+        # -> 16 x 16 x  64 x B
+        ConvTranspose((4, 4),  32 =>  3; stride=(2, 2), pad=(1, 1)), x -> σs[4].(x)
+        # -> 32 x 32 x   3 x B
+    )
+end
+
+build_convnet_outcifar(Din::Int, σ::Function, σlast::Function; kwargs...) = build_convnet_outcifar(Din, (σ, σ, σ, σlast); kwargs...)
+
+# TODO: unify building conv archs
 function NeuralSampler(
     base, 
     Dz::Int, 
-    Dhs::IntIte, 
-    Dx::Int, 
+    Dhs::Union{IntIte,String}, 
+    Dx::Union{Int,Tuple{Int,Int,Int}}, 
     σ::Function, 
     σlast::Function, 
     isnorm::Bool, 
     n_default::Int
 )
     size(base) != (Dz,) && throw(DimensionMismatch("size(base) ($(size(base))) != (Dz,) ($((Dz,)))"))
-    return NeuralSampler(base, DenseNet(Dz, Dhs, Dx, σ, σlast; isnorm=isnorm), n_default)
+    if Dx == (32, 32, 3) && Dhs == "conv"
+        f = build_convnet_outcifar(Dz, σ, σlast; isnorm=isnorm)
+    elseif Dx == (28, 28, 1) && Dhs == "conv"
+        f = build_conv_outmnist(Dz, σ, σlast; isnorm=isnorm)
+    else
+        f = DenseNet(Dz, Dhs, Dx, σ, σlast; isnorm=isnorm)
+    end
+    return NeuralSampler(base, f, n_default)
 end
 
 ### Projector
@@ -50,9 +106,14 @@ function DenseProjector(Dx::Int, Dhs::IntIte, Df::Int, σ::Function, isnorm::Boo
     return Projector(DenseNet(Dx, Dhs, Df, σ, identity; isnorm=isnorm), Df)
 end
 
-function ConvProjector(Dx::Int, Df::Int, σ::Function, isnorm::Bool)
-    @assert Dx == 28 * 28 "[ConvProjector] Only MNIST-like data is supported."
-    return Projector(ConvNet((28, 28, 1), Df, σ, identity; isnorm=isnorm), Df)
+function ConvProjector(Dx::Union{Int,Tuple{Int,Int,Int}}, Df::Int, σ::Function, isnorm::Bool)
+    if Dx == 28 * 28 || Dx == (28, 28, 1)
+        return Projector(ConvNet((28, 28, 1), Df, σ, identity; isnorm=isnorm), Df)
+    elseif Dx == (32, 32, 3)
+        return Projector(ConvNet((32, 32, 3), Df, σ, identity; isnorm=isnorm), Df)
+    else
+        @error "[ConvProjector] Only MNIST-like or CIFAR-like data is supported."
+    end
 end
 
 ### Discriminator
@@ -61,9 +122,14 @@ function DenseDiscriminator(Dx::Int, Dhs::IntIte, σ::Function, isnorm::Bool)
     return Discriminator(DenseNet(Dx, Dhs, 1, σ, sigmoid; isnorm=isnorm))
 end
 
-function ConvDiscriminator(Dx::Int, σ::Function, isnorm::Bool)
-    @assert Dx == 28 * 28 "[ConvDiscriminator] Only MNIST-like data is supported."
-    return Discriminator(ConvNet((28, 28, 1), 1, σ, sigmoid; isnorm=isnorm))
+function ConvDiscriminator(Dx::Union{Int,Tuple{Int,Int,Int}}, σ::Function, isnorm::Bool)
+    if Dx == 28 * 28
+        return Discriminator(ConvNet((28, 28, 1), 1, σ, sigmoid; isnorm=isnorm))
+    elseif Dx == 32 * 32 * 3
+        return Discriminator(ConvNet((32, 32, 3), 1, σ, sigmoid; isnorm=isnorm))
+    else
+        @error "[ConvDiscriminator] Only MNIST-like or CIFAR-like data is supported."
+    end
 end
 
 include("models.jl")
@@ -71,10 +137,18 @@ export GAN, MMDNet, RMMMDNet, train!, evaluate
 
 ###
 
+flatten(x) = reshape(x, :, last(size(x)))
+
 function get_data(name::String)
     rng = MersenneTwister(1)
     if name == "mnist"
-        Xtrain = Array{Float32,2}(hcat(float.(reshape.(MNIST.images(), :))...))
+        Xtrain = permutedims(MNIST.traintensor(Float32), (2, 1, 3))
+        Xtrain = flatten(Xtrain)
+#         Xtrain = reshape(Xtrain, (28, 28, 1, last(size(Xtrain))))
+    end
+    if name == "cifar10"
+        Xtrain = permutedims(CIFAR10.traintensor(Float32), (2, 1, 3, 4))
+        Xtrain = Xtrain .* 2 .- 1
     end
     if name == "gaussian"
         N = 1_000
@@ -84,18 +158,25 @@ function get_data(name::String)
         N = 1_000
         Xtrain = rand(rng, Ring(8, 2, 2f-1), N)
     end
-    return Dataset(gpu(Xtrain); name=name)
+    Xtrain = Xtrain |> gpu
+    return Dataset(Xtrain; name=name)
 end
 
 function plot!(d::Dataset, g::NeuralSampler)
     rng = MersenneTwister(1)
-    if d.name == "mnist" 
-        Xdata = d.train |> cpu
-        Xgen = rand(rng, g, 32) |> cpu |> Tracker.data
-        MLToolkit.plot!(GrayImages(hcat(Xdata[:,shuffle(rng, 1:last(size(Xdata)))[1:32]], Xgen)))
+    if d.name == "mnist"
+        Xdata = d.train
+        Xgen = rand(rng, g, 32) |> cpu
+        MLToolkit.plot!(GrayImages(cat(Xdata[:,shuffle(rng, 1:last(size(Xdata)))[1:32]], Xgen; dims=2)))
+#         MLToolkit.plot!(GrayImages(dropdims(cat(Xdata[:,:,:,shuffle(rng, 1:last(size(Xdata)))[1:32]], Xgen; dims=4); dims=3)))
+    elseif d.name == "cifar10"
+        Xdata = d.train
+        Xgen = rand(rng, g, 32) |> cpu
+        Xshow = cat(Xdata[:,:,:,shuffle(rng, 1:last(size(Xdata)))[1:32]], Xgen; dims=4)
+        MLToolkit.plot!(RGBImages((Xshow .+ 1) ./ 2))
     elseif d.name in ["gaussian", "ring"]
-        Xdata = d.train |> cpu
-        Xgen = rand(rng, g, last(size(Xdata))) |> cpu |> Tracker.data
+        Xdata = d.train
+        Xgen = rand(rng, g, last(size(Xdata))) |> cpu
         plt.scatter(Xdata[1,:], Xdata[2,:], marker=".", label="data", alpha=0.5)
         plt.scatter(Xgen[1,:],  Xgen[2,:],  marker=".", label="gen",  alpha=0.5)
         autoset_lim!(Xdata)
@@ -105,22 +186,14 @@ end
 
 function plot!(d::Dataset, g::NeuralSampler, f::Projector)
     rng = MersenneTwister(1)
-    if d.name == "mnist" 
-        Xdata = d.train |> cpu
-        Xgen = rand(rng, g, 32) 
-        fXdata = f(Xdata) |> cpu |> Tracker.data
-        fXgen = f(Xgen) |> cpu |> Tracker.data
-        MLToolkit.plot!(GrayImages(hcat(fXdata[:,shuffle(rng, 1:last(size(fXdata)))[1:32]], fXgen)))
-    elseif d.name in ["gaussian", "ring"]
-        Xdata = d.train |> cpu
-        Xgen = rand(rng, g, last(size(Xdata)))
-        fXdata = f(Xdata) |> cpu |> Tracker.data
-        fXgen = f(Xgen) |> cpu |> Tracker.data
-        plt.scatter(fXdata[1,:], fXdata[2,:], marker=".", label="data", alpha=0.5)
-        plt.scatter(fXgen[1,:],  fXgen[2,:],  marker=".", label="gen",  alpha=0.5)
-        autoset_lim!(fXdata)
-        plt.legend(fancybox=true, framealpha=0.5)
-    end
+    Xdata = d.train
+    Xgen = rand(rng, g, last(size(Xdata)))
+    fXdata = f(Xdata) |> cpu
+    fXgen = f(Xgen) |> cpu
+    plt.scatter(fXdata[1,:], fXdata[2,:], marker=".", label="data", alpha=0.5)
+    plt.scatter(fXgen[1,:],  fXgen[2,:],  marker=".", label="gen",  alpha=0.5)
+    autoset_lim!(fXdata)
+    plt.legend(fancybox=true, framealpha=0.5)
 end
 
 parse_csv(T, l) = map(x -> parse(T, x), split(l, ","))
@@ -141,10 +214,21 @@ function get_model(args::NamedTuple, dataset::Dataset)
     elseif args.base == "gaussian"
         base = GaussianNoise(args.Dz)
     end
-    Dhs_g = parse_csv(Int, args.Dhs_g)
-    act = parse_op(args.act)
-    actlast = parse_op(args.actlast)
-    g = NeuralSampler(base, args.Dz, Dhs_g, dim(dataset), act, actlast, args.norm, args.batchsize_g)
+    if args.Dhs_g == "conv"
+        if :act in keys(args)
+            @warn "args.act is ignored"
+        end
+        if :actlast in keys(args)
+            @warn "args.actlast is ignored"
+        end
+        actlast = dataset.name == "cifar10" ? tanh : sigmoid
+        g = NeuralSampler(base, args.Dz, "conv", dim(dataset), relu, actlast, args.norm, args.batchsize_g)
+    else
+        Dhs_g = parse_csv(Int, args.Dhs_g)
+        act = parse_op(args.act)
+        actlast = parse_op(args.actlast)
+        g = NeuralSampler(base, args.Dz, Dhs_g, dim(dataset), act, actlast, args.norm, args.batchsize_g)
+    end
     if args.modelname == "gan"
         if args.Dhs_d == "conv"
             d = ConvDiscriminator(dim(dataset), act, args.norm)
@@ -159,29 +243,36 @@ function get_model(args::NamedTuple, dataset::Dataset)
             m = MMDNet(logger, opt, sigma, g)
         elseif args.modelname == "rmmmdnet"
             if args.Dhs_f == "conv"
+                act = x -> leakyrelu(x, 2f-1)
+                if :act in keys(args)
+                    @warn "args.act is ignored"
+                end
                 f = ConvProjector(dim(dataset), args.Df, act, args.norm)
             else
                 Dhs_f = parse_csv(Int, args.Dhs_f)
                 f = DenseProjector(dim(dataset), Dhs_f, args.Df, act, args.norm)
             end
-            m = RMMMDNet(logger, opt, sigma, g, f) |> track
+            m = RMMMDNet(logger, opt, sigma, g, f)
         end
     end
     @info "Init $(args.modelname) with $(nparams(m) |> Humanize.digitsep) parameters" logdir
     return m |> gpu
 end
 
-function run_exp(args; model=nothing)
+function run_exp(args; model=nothing, initonly=false)
     seed!(args.seed)
     
     data = get_data(args.dataset)
     dataloader = DataLoader(data, args.batchsize)
 
     model = isnothing(model) ? get_model(args, data) : model
-
-    with_logger(model.logger) do
-        train!(model, dataloader, args.n_epochs; evalevery=100)
+    
+    if !initonly
+        with_logger(model.logger) do
+            train!(model, dataloader, args.n_epochs; evalevery=50)
+        end
     end
+    
     evaluate(model, dataloader)
     
     modelpath = savemodel(model)
--- src/models.jl
+++ src/models.jl
@@ -1,11 +1,16 @@
 using DensityRatioEstimation: DensityRatioEstimation, gaussian_gram_by_pairwise_sqd, pairwise_sqd, MMDAnalytical, _estimate_ratio
 
-function DensityRatioEstimation.adddiag(mmd::MMDAnalytical{T, Val{:true}}, Kdede::Union{MLToolkit.Flux.CuArray,MLToolkit.Tracker.TrackedArray}) where {T}
-    ϵ = diagm(0 => mmd.ϵ * fill(one(T), size(Kdede, 1)))
-    return Kdede + gpu(ϵ)
-end
+makecuϵ(T, D, ϵ) = MLToolkit.Flux.CuArray(diagm(0 => ϵ * fill(one(T), D)))
+
+Flux.Zygote.@nograd makecuϵ
+
+DensityRatioEstimation.adddiag(mmd::MMDAnalytical{T,Val{true}}, Kdede) where {T} = 
+    Kdede + makecuϵ(T, size(Kdede, 1), mmd.ϵ)
 
 _compute_mmd_sq(Kdede, Kdenu, Knunu) = mean(Kdede) - 2mean(Kdenu) + mean(Knunu)
+
+EPS_RATIO = 1f-3
+EPS_MMD = 1f-6
 
 function compute_mmd(x_de, x_nu; σs=[], verbose=false)
     function f(pdot_dede, pdot_denu, pdot_nunu, σ)
@@ -15,14 +20,14 @@ function compute_mmd(x_de, x_nu; σs=[], verbose=false)
         return _compute_mmd_sq(Kdede, Kdenu, Knunu)
     end
     mmd_sq = multi_run(f, x_de, x_nu, σs, verbose)
-    return sqrt(mmd_sq + 1f-6)
+    return sqrt(mmd_sq + EPS_MMD)
 end
 
 function estimate_ratio(x_de, x_nu; σs=[], verbose=false)
     function f(pdot_dede, pdot_denu, pdot_nunu, σ)
         Kdede = gaussian_gram_by_pairwise_sqd(pdot_dede, σ)
         Kdenu = gaussian_gram_by_pairwise_sqd(pdot_denu, σ)
-        return _estimate_ratio(MMDAnalytical(1f-3), Kdede, Kdenu)
+        return _estimate_ratio(MMDAnalytical(EPS_RATIO), Kdede, Kdenu)
     end
     return multi_run(f, x_de, x_nu, σs, verbose) / convert(Float32, length(σs))
 end
@@ -32,12 +37,12 @@ function estimate_ratio_compute_mmd(x_de, x_nu; σs=[], verbose=false)
         Kdede = gaussian_gram_by_pairwise_sqd(pdot_dede, σ)
         Kdenu = gaussian_gram_by_pairwise_sqd(pdot_denu, σ)
         Knunu = gaussian_gram_by_pairwise_sqd(pdot_nunu, σ)
-        return (_estimate_ratio(MMDAnalytical(1f-3), Kdede, Kdenu), _compute_mmd_sq(Kdede, Kdenu, Knunu))
+        return (_estimate_ratio(MMDAnalytical(EPS_RATIO), Kdede, Kdenu), _compute_mmd_sq(Kdede, Kdenu, Knunu))
     end
     ratio, mmd_sq = multi_run(f, x_de, x_nu, σs, verbose)
     return (
         ratio=ratio / convert(Float32, length(σs)), 
-        mmd=sqrt(mmd_sq + 1f-6)
+        mmd=sqrt(mmd_sq + EPS_MMD)
     )
 end
 
@@ -56,21 +61,21 @@ function multi_run(f_run, x_de, x_nu, σs, verbose)
     pdot_nunu = pairwise_sqd(x_nu)
     
     if isempty(σs)
-        σ = sqrt(median(vcat(vec.(Tracker.data.([pdot_dede, pdot_denu, pdot_nunu])))))
+        σ = sqrt(median(vcat(vec.([pdot_dede, pdot_denu, pdot_nunu]))))
         multi_run_verbose(σ, verbose)
         σs = [σ]
     end
     
-    res = f_run(pdot_dede, pdot_denu, pdot_nunu, σs[1])
-    for σ in σs[2:end]
-        _res = f_run(pdot_dede, pdot_denu, pdot_nunu, σ)
-        if res isa Tuple
-            res = tuple((res[i] .+ _res[i] for i in 1:length(res))...)
+    local res = nothing
+    for σ in σs
+        res_current = f_run(pdot_dede, pdot_denu, pdot_nunu, σ)
+        if isnothing(res)
+            res = res_current
         else
-            res += _res
+            res = res .+ res_current
         end
     end
-
+    
     return res
 end
 
@@ -91,7 +96,7 @@ Flux.functor(m::MMDNet) = (m.g,), t -> MMDNet(m.logger, m.opt,  m.σs, t[1])
 
 function MLToolkit.Neural.loss(m::MMDNet, x_data)
     x_gen = rand(m.g)
-    mmd = compute_mmd(x_gen, x_data; σs=m.σs)
+    mmd = compute_mmd(x_gen |> flatten, x_data |> flatten; σs=m.σs)
     loss_g = mmd
     return (loss_g=loss_g, mmd=mmd,)
 end
@@ -156,9 +161,6 @@ evaluate(m::GAN, dl) = evaluate_g(m.g, dl.dataset)
 
 # RMMMDNet
 
-const istraining = Ref(:false)
-Flux.istraining() = istraining[]
-
 struct RMMMDNet <: AbstractGenerativeModel
     logger
     opt
@@ -170,44 +172,40 @@ end
 Flux.functor(m::RMMMDNet) = (m.g, m.f), t -> RMMMDNet(m.logger, m.opt, m.σs, t[1], t[2])
 
 function update!(opt, m::RMMMDNet, x_data)
-    # Training mode
-    istraining[] = true
-    ps_g = trackerparams(m.g)
-    ps_f = trackerparams(m.f)
-
-    # Forward
-    x_gen = rand(m.g)
-    fx_gen, fx_data = m.f(x_gen), m.f(x_data)
-    ratio, mmd = estimate_ratio_compute_mmd(fx_gen, fx_data; σs=m.σs)
-    pearson_divergence = mean((ratio .- 1) .^ 2)
-    raito_mean = mean(ratio)
-    loss_f = -(pearson_divergence + raito_mean)
-    loss_g = mmd
-
-    # Collect gradients
-    gs_f = gradient(() -> loss_f, ps_f; once=false)
-    Tracker.zero_grad!.(Tracker.grad.(ps_g)) # gradient call above leads to un-zeroed gradients in `ps_g`
-    
-    gs_g = gradient(() -> loss_g, ps_g)
-    Tracker.zero_grad!.(Tracker.grad.(ps_f)) # gradient call above leads to un-zeroed gradients in `ps_f`
-    
     # Update f and g
+    ps_f, ps_g = Flux.params(m.f), Flux.params(m.g)
+    
+    local x_gen, ratio, pearson_divergence, raito_mean, mmd, loss_f, loss_g
+    gs_f, gs_g = gradient(ps_f, ps_g) do
+        x_gen = rand(m.g)
+        fx_gen, fx_data = m.f(x_gen), m.f(x_data)
+        ratio, mmd = estimate_ratio_compute_mmd(fx_gen, fx_data; σs=m.σs)
+        loss_g = mmd
+        # Clip version
+        ratio = clamp.(ratio, 0f0, 1f9)
+        pearson_divergence = mean((ratio .- 1) .^ 2)
+        loss_f = -pearson_divergence
+        # Regularizer version
+#         raito_mean = mean(ratio)
+#         pearson_divergence = mean((ratio .- 1) .^ 2)
+#         loss_f = -(pearson_divergence + raito_mean)
+        (loss_f, loss_g)
+    end
+
     Flux.Optimise.update!(opt, ps_f, gs_f)
     Flux.Optimise.update!(opt, ps_g, gs_g)
 
-    ratio_orig = estimate_ratio(x_gen |> Tracker.data, x_data; σs=m.σs)
     return (
-        squared_distance=mean((ratio_orig - Tracker.data(ratio)) .^ 2),
         pearson_divergence=pearson_divergence,
-        raito_mean=raito_mean,
-        loss_f=loss_f,
+#         raito_mean=raito_mean,
         mmd=mmd,
+        loss_f=loss_f,
         loss_g=loss_g,
+#         squared_distance=mean((estimate_ratio(x_gen |> flatten, x_data |> flatten; σs=m.σs) - ratio) .^ 2),
     )
 end
 
 function evaluate(m::RMMMDNet, dl)
-    istraining[] = false
     fig_g = plt.figure(figsize=(3.5, 3.5))
     plot!(dl.dataset, m.g)
     if m.f.Dout != 2    # only visualise projector if its output dim is 2
